845664f11b55226140755b473d9385bd3573e05a
BPM17 <barucpm17@gmail.com>
2024-12-20 11:20:47 -0600

    Minor changes as format, margin and tying to link models with db it still pending

--- bpmcl/coverLetter/views.py
+++ bpmcl/coverLetter/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from .models import item
 
 # Create your views here.
 def index(request):
+    # dbitems = item.objects.all()
+    # context = {'dbitems':dbitems}
     return render(request, 'index.html')
