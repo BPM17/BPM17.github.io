2a03c8d2afbcd616e81956b8693d784d5b14e0ae
BPM17 <barucpm17@gmail.com>
2025-04-16 19:42:39 -0600

    Changes added to the coverletter, experience section added from de DB with jinja loops

--- bpmcl/coverLetter/admin.py
+++ bpmcl/coverLetter/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ProjectItem, EducationItem, ExperienceItem
 
 # Register your models here.
+admin.site.register(ProjectItem)
+admin.site.register(ExperienceItem)
+admin.site.register(EducationItem)
